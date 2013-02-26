2b14212a2a5a2eadeb41ae733295be3ee6315647
Sjors <github@sjorsbaltus.nl>
2013-02-26 13:16:50 +0100

    Moved the path (home directory definition) from the shell to the config

--- lib/gitlab/backend/shell.rb
+++ lib/gitlab/backend/shell.rb
@@ -58,7 +58,7 @@ module Gitlab
     end
    
     def gitlab_shell_user_home
-      File.expand_path("~#{Gitlab.config.gitlab_shell.directory}")
+      File.expand_path("#{Gitlab.config.gitlab_shell.directory}")
     end
 
   end
