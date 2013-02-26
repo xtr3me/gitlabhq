31bc2cce2630556216a7a9125fbc534e681f0e61
Sjors <github@sjorsbaltus.nl>
2013-02-26 12:31:29 +0100

    Make the grant emails to the users configurable

--- config/initializers/1_settings.rb
+++ config/initializers/1_settings.rb
@@ -54,6 +54,7 @@ Settings.gitlab['relative_url_root'] ||= ''
 Settings.gitlab['protocol']   ||= Settings.gitlab.https ? "https" : "http"
 Settings.gitlab['email_from'] ||= "gitlab@#{Settings.gitlab.host}"
 Settings.gitlab['support_email']  ||= Settings.gitlab.email_from
+Settings.gitlab['send_grant_emails']  ||= true
 Settings.gitlab['url']        ||= Settings.send(:build_gitlab_url)
 Settings.gitlab['user']       ||= 'git'
 Settings.gitlab['signup_enabled'] ||= false
